06f4604b14bbacea7ad76c23159581d5829b61a1
lukewhiley <luke.whiley@murdoch.edu.au>
2021-02-07 21:25:15 +0800

    improvements (currently working)

--- App.R
+++ App.R
@@ -1,44 +1,81 @@
 library(shiny)
+library(janitor)
+library(tidyverse)
 library(png)
 library(vroom)
 
 # ui object
 ui_title <- titlePanel(title=div(img(src="ANPC_logo.png", width = "240px", height = "140px"), 
                                    "LGW - Metabolomics Workbook", style = "color:#3474A7"))
-  
-ui_upload <- sidebarLayout(
+
+#create a user interface for uplopading the metabolite data values
+ui_data_upload <- sidebarLayout(
   sidebarPanel(
-    fileInput("file", NULL, accept = c(".csv", ".tsv")),
+    p("select a .csv or .tsv containing metabolite concentration values. Column headers should contain the metabolite name matching the metabolite list below. sample/filenames should be in a column called: sample name"),
+    fileInput("metabolite_data_file", NULL, accept = c(".csv", ".tsv")),
     numericInput("r", "Rows to preview", 10, min = 1),
     numericInput("c", "Columns to preview", 5, min = 1)
   ),
   mainPanel(
     h3("Raw data"),
-    tableOutput("head")
+    tableOutput("metabolite_data_file")
+  )
+)
+
+#create a user interface for uploading a list of target metabolites
+ui_metabolite_target_upload <- sidebarLayout(
+  sidebarPanel(
+    p("select a .csv or .tsv contatining a list of metabolite targets in a column named: metabolite"),
+    fileInput("metabolite_target_file", NULL, accept = c(".csv", ".tsv")),
+    numericInput("r", "Rows to preview", 5, min = 1),
+  ),
+  mainPanel(
+    h3("Metabolite targets"),
+    tableOutput("metabolite_target_file")
   )
 )
 
 ui <- fluidPage(
   ui_title,
-  ui_upload
+  ui_data_upload,
+  ui_metabolite_target_upload
 )
 
 
 # server()
 server <- function(input, output, session) {
-  data <- reactive({
-    req(input$file)
+  
+  #server on importing raw data
+  metabolite_data_file <- reactive({
+    req(input$metabolite_data_file)
     
-    ext <- tools::file_ext(input$file$name)
+    ext <- tools::file_ext(input$metabolite_data_file$name)
     switch(ext,
-           csv = vroom::vroom(input$file$datapath, delim = ","),
-           tsv = vroom::vroom(input$file$datapath, delim = "\t"),
+           csv = vroom::vroom(input$metabolite_data_file$datapath, delim = ","),
+           tsv = vroom::vroom(input$metabolite_data_file$datapath, delim = "\t"),
            validate("Invalid file; Please upload a .csv or .tsv file")
     )
   })
   
-  output$head <- renderTable({
-    head(data(), c(input$r, input$c))
+  output$metabolite_data_file <- renderTable({
+    head(metabolite_data_file(), c(input$r, input$c))
+    
+  })
+  
+  #server on importing metabolite target list
+  metabolite_target_file <- reactive({
+    req(input$metabolite_target_file)
+    
+    ext <- tools::file_ext(input$metabolite_target_file$name)
+    switch(ext,
+           csv = vroom::vroom(input$metabolite_target_file$datapath, delim = ","),
+           tsv = vroom::vroom(input$metabolite_target_file$datapath, delim = "\t"),
+           validate("Invalid file; Please upload a .csv or .tsv file")
+    )
+  })
+  
+  output$metabolite_target_file <- renderTable({
+    head(metabolite_target_file(), input$r)
     
   })
 }
