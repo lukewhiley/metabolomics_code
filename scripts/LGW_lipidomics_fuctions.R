aeeb9253232a9bebbe798c55119392b2a4d0e4ef
Luke Whiley <luke.whiley@murdoch.edu.au>
2021-03-28 21:57:51 +0800

    updated lipidomics script to make more use of functions

--- scripts/LGW_lipidomics_fuctions.R
+++ scripts/LGW_lipidomics_fuctions.R
@@ -1,17 +1,36 @@
+# create family data
+
+
+create_family_data_summed <- function(individual_lipid_data){
+ apply(lipid_family, 1, function(func_family){
+  #browser()
+  family_targets <- which(sub("\\(.*", "", colnames(training_analysis_data)) == func_family) # find the columns in each family
+  temp_family_data <- individual_lipid_data %>% select(family_targets)  %>% mutate(rowsum = rowSums(.)) %>% select(rowsum)
+  colnames(temp_family_data) <- func_family
+  temp_family_data
+}) %>% bind_cols()
+
+#colnames(temp_family_data) <- c(lipid_family$lipid_class)
+}
+
+
 ######### PCA analysis ##########
 
 # pca plot using metaboanalyst
 
 lipids_pca <- function(multivariate_data){
-  if(length(grep("TAG", colnames(multivariate_data))) > 1){ 
-  pca_x <- multivariate_data %>%  select(c(lipid)) %>% as.matrix()
-}
 
-if(length(grep("TAG", colnames(multivariate_data))) == 1){ 
-   pca_x <- multivariate_data %>%  select(c(lipid_family$lipid_class)) %>% as.matrix()
-   }
+  if(length(grep("TAG", colnames(multivariate_data))) > 1){ 
+    pca_x <- multivariate_data %>%  select(c(lipid)) %>% as.matrix()
+    title_text <- "individual lipid species"
+  }
   
-pca_class <- multivariate_data %>% select(class) %>% as.matrix()
+  if(length(grep("TAG", colnames(multivariate_data))) == 1){ 
+    pca_x <- multivariate_data %>%  select(c(lipid_family$lipid_class)) %>% as.matrix()
+    title_text <- "lipid family"
+  }
+  
+  pca_class <- multivariate_data %>% select(class) %>% as.matrix()
   sampleID <- multivariate_data %>% select(sampleID)
   
   pca_model <- pca(pca_x, scale = "UV", center = TRUE)
@@ -38,7 +57,7 @@ pca_class <- multivariate_data %>% select(class) %>% as.matrix()
     theme(legend.text = element_text(size = 18)) +
     theme(legend.title  = element_text(size = 18))+
     theme(legend.position = "bottom") + 
-    ggtitle(paste(project_name, " PCA - individual lipid species", sep = ""))
+    ggtitle(paste(project_name, " PCA - ", title_text, sep = ""))
   
   if(label_sampleIDs == TRUE){
     pca_plot <- pca_plot + geom_text(data=plot_Val, aes(x =  PC1, y =  PC2, label = sampleID))
@@ -50,40 +69,103 @@ pca_class <- multivariate_data %>% select(class) %>% as.matrix()
     theme_bw() +
     xlab("PC1")+
     ylab("PC2")+
-    ggtitle("PCA Loadings - individual lipid species")+
     theme(plot.title = element_text(hjust = 0.5)) +
     theme(plot.title = element_text(size=30)) +
     theme(axis.text.y=element_text(size = 22, margin = margin(t = 0, r = 0, b = 0, l = 2))) +
     theme(axis.text.x=element_text(size = 22)) +
     theme(axis.title = element_text(size = 25)) +
-    theme(legend.position = "none"); pca_plot_loadings
+    theme(legend.position = "none") +
+    ggtitle(paste(project_name, " PCA - ", title_text, sep = ""))
   
   pca_plot_list <- list(pca_plot, pca_plot_loadings)
   print(pca_plot_list)
-  
-
 }
 
 
 #interactive plotly PCA plots
 
 lipids_pca_plotly <- function(multivariate_data){
-    if(length(grep("TAG", colnames(multivariate_data))) > 1){ 
-      pca_x <- multivariate_data %>%  select(c(lipid)) %>% as.matrix()
-    }
-    
-    if(length(grep("TAG", colnames(multivariate_data))) == 1){ 
-      pca_x <- multivariate_data %>%  select(c(lipid_family$lipid_class)) %>% as.matrix()
-    }
+ 
+  if(length(grep("TAG", colnames(multivariate_data))) > 1){ 
+    pca_x <- multivariate_data %>%  select(c(lipid)) %>% as.matrix()
+    title_text <- "individual lipid species"
+  }
+  
+  if(length(grep("TAG", colnames(multivariate_data))) == 1){ 
+    pca_x <- multivariate_data %>%  select(c(lipid_family$lipid_class)) %>% as.matrix()
+    title_text <- "lipid family"
+  }
+  
   pca_class <- multivariate_data %>% select(class) %>% as.matrix()
   sampleID <- multivariate_data %>% select(sampleID)
+  
   pca_model <- pca(pca_x, scale = "UV", center = TRUE)
+  PC1 <- as.numeric(as.matrix(pca_model@t[,1]))
+  PC2 <- as.numeric(as.matrix(pca_model@t[,2]))
+  
+  #produce PCA plot
+  plot_Val <- as_tibble(cbind(PC1, PC2))
+  plot_Val$sampleID <- sampleID$sampleID
+  plot_Val$class <- pca_class
   
   plotly_loadings_data <- pca_model@p %>% as_tibble(rownames = "lipid") %>% rename(PC1 = V1, PC2 = V2)
-  
-  plotly_pca <- plot_ly(plot_Val, x = ~PC1, y = ~PC2, text = ~sampleID, color = ~class, colors = plot_two_group_colours, marker = list(size = 12)); plotly_pca
-  plotly_loadings <- plot_ly(plotly_loadings_data, x = ~PC1, y = ~PC2, text = ~lipid, colors = "black");plotly_loadings
-  combined_plotly <- subplot(plotly_pca, plotly_loadings) %>% layout(showlegend = FALSE)
+
+  plotly_pca <- plot_ly(plot_Val, x = ~PC1, y = ~PC2, text =~sampleID, color = ~class, colors = plot_two_group_colours, marker = list(size = 12)) %>% 
+    layout(title = paste(project_name, " Plotly PCA - ", title_text, sep = ""))
+  plotly_loadings <- plot_ly(plotly_loadings_data, x = ~PC1, y = ~PC2, text = ~lipid, marker = list(color = "black")) %>% 
+    layout(title = paste(project_name, " Plotly PCA - ", title_text, sep = ""))
+  combined_plotly <- subplot(plotly_pca, plotly_loadings) %>% layout(showlegend = FALSE, title =  "Plotly PCA")
   pca_plotly_list <- list(plotly_pca, plotly_loadings, combined_plotly)
-  print(pca_plotly_list)
+  print(pca_plotly_list) 
+}
+
+
+###### OPLS-DA function #####
+
+lipids_opls <- function(multivariate_data){
+  # browser()
+  if(length(grep("TAG", colnames(multivariate_data))) > 1){ 
+    pca_x <- multivariate_data %>%  select(c(lipid)) %>% as.matrix()
+  }
+  
+  if(length(grep("TAG", colnames(multivariate_data))) == 1){ 
+    pca_x <- multivariate_data %>%  select(c(lipid_family$lipid_class)) %>% as.matrix()
+  }
+
+  opls_x <-  multivariate_data %>% select(c(lipid)) %>% as.matrix() 
+  opls_y <- multivariate_data %>% select(class) %>% as.matrix()
+  sampleID <- multivariate_data %>% select(sampleID)
+  
+  opls_model <- opls(log10(opls_x+1), opls_y, center = TRUE, scale = "UV")
+  plot_t_pred <- as.numeric(as.matrix(opls_model@t_pred))
+  plot_t_orth <- as.numeric(as.matrix(opls_model@t_orth))
+  
+  #produce OPLS-DA plot
+  plot_Val <- as_tibble(cbind(plot_t_pred, plot_t_orth))
+  plot_Val$class <- opls_y[,'class']
+  plot_Val$sampleID <- sampleID$sampleID
+  
+  opls_plot <- ggplot() +
+    geom_vline(xintercept = 0, colour="black", linetype = "longdash", alpha = 0.4)+
+    geom_point(data=plot_Val, aes(x =  plot_t_pred, y =  plot_t_orth, color = class),  size = 6.0)+
+    theme_bw() +
+    scale_color_manual(values=plot_two_group_colours)+
+    xlab("t_pred")+
+    ylab("t_orth")+
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(plot.title = element_text(size=30)) +
+    theme(axis.text.y=element_text(size = 22, margin = margin(t = 0, r = 0, b = 0, l = 2))) +
+    theme(axis.text.x=element_text(size = 22)) +
+    theme(axis.title = element_text(size = 25)) +
+    theme(legend.position = 'none') +
+    ggtitle(paste(project_name, " OPLS-DA training model - individual lipid species", sep = ""))
+  
+  if(label_sampleIDs == TRUE){
+    opls_plot <- opls_plot + geom_text(data=plot_Val, aes(x =  plot_t_pred, y =  plot_t_orth, label = sampleID))
+  }
+  
+  opls_plot_loadings <- eruption(opls_model)
+  
+  opls_plot_list <- list(opls_plot, opls_plot_loadings)
+  print(opls_plot_list)
 }
