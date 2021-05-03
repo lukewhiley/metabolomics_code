01207272e74225e6133ae9f8f5ea6f60231f1503
Luke Whiley <luke.whiley@murdoch.edu.au>
2021-05-03 12:24:47 +0800

    edited

--- scripts/reading_script_from_github.r
+++ scripts/reading_script_from_github.r
@@ -6,7 +6,6 @@ library(httr)
 script <-
   GET(
     url = "https://raw.githubusercontent.com/lukewhiley/metabolomics_code/main/lgw_boxplots.r",
-    authenticate("luke.whiley@murdoch.edu.au", "cda3533bb0a8248333824bd91029b48c367164d4")  ,   # Instead of PAT, could use password
     accept("application/vnd.github.v3.raw")
   ) %>% content(as = "text")
 
