edd7a8fb39d7b75fc8c1f032107a678362ec62a3
glm729 <georgemalone@outlook.com.au>
2021-06-01 17:09:11 +0800

    Restructured scripts/univariate_stats.r
    - Abstracted `lapply` functions
    - One undefined variable used in both `lapply` calls
    - Boxplot title shifted to variable `main` in call to `boxplot`
    - Added a bit of commentary
    - Untested (no data), so may not work as before

--- scripts/univariate_stats.r
+++ scripts/univariate_stats.r
@@ -1,35 +1,64 @@
-# 
+#!/usr/bin/env Rscript
+
+
+# Required libraries
+# -----------------------------------------------------------------------------
+
+
 library(tidyverse)
 library(janitor)
+
+
+# Function definitions
+# -----------------------------------------------------------------------------
+
+
+resultListFunc <- function(func_stats, data) {
+  # FIXME:
+  # - UNDEFVAR treatment_allocation_blind
+  func_data <- data %>% select(treatment_allocation_blind, all_of(func_stats))
+  colnames(func_data) <- c("treatment", "metabolite")
+  mw_res <- wilcox.test(
+    metabolite ~ as.factor(treatment),
+    data = func_data,
+    exact = FALSE)
+  return(mw_res$p.value)
+}
+
+
+boxplotListFunc <- function(func_stats, data) {
+  # FIXME:
+  # - UNDEFVAR treatment_allocation_blind
+  func_data <- data %>% select(treatment_allocation_blond, all_of(func_stats))
+  colnames(func_data) <- c("treatment", "metabolite")
+  boxplot <- boxplot(
+    metabolite ~ as.factor(treatment),
+    data = func_data,
+    main = paste(func_stats))
+  return(boxplot)
+}
+
+
+# Operations
+# -----------------------------------------------------------------------------
+
+
+# Choose a file to parse as CSV, and clean the names
 data_a <- read_csv(file = file.choose()) %>% clean_names()
 
+# Subset the data to the metabolite data
 metabolite_list <- names(data_a)[2:ncol(data_a)]
 
-MW_resultList <- lapply(metabolite_list, function(FUNC_STATS){
-  #browser()
-  func_data <- data_a %>% select(treatment_allocation_blind, all_of(FUNC_STATS))
-  colnames(func_data) <- c("treatment", "metabolite")
-  mw_res <- wilcox.test(metabolite ~ as.factor(treatment),
-                        data = func_data,
-                        exact=FALSE
-                        )
-  mw_res$p.value
-}) %>% unlist() %>% c() %>% as_tibble() %>% add_column(metabolite_list, .before = 1) %>% arrange(value)
+# Process data and reformat as Tibble
+MW_resultList <- lapply(metabolite_list, resultListFunc, data = data_a) %>%
+  unlist() %>%
+  c() %>%
+  as_tibble() %>%
+  add_column(metabolite_list, .before = 1) %>%
+  arrange(value)
 
+# Replace the column names of the result list Tibble
 colnames(MW_resultList) <- c("metabolite", "pvalue")
 
-MW_resultList
-
-MW_boxplotList <- lapply(metabolite_list, function(FUNC_STATS){
-  browser()
-  func_data <- data_a %>% select(treatment_allocation_blind, all_of(FUNC_STATS))
-  colnames(func_data) <- c("treatment", "metabolite")
-  boxplot(metabolite ~ as.factor(treatment),
-         data = func_data
-         )
-  title(paste(FUNC_STATS))
-  
-})
-
-
-
+# Produce boxplots of each group in the metabolite list
+MW_boxplotList <- lapply(metabolite_list, boxplotListFunc, data = data_a)
