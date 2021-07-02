660b14614fac28c1578f08019f084569d0af2ac2
Luke Whiley <luke.whiley@murdoch.edu.au>
2021-07-02 14:09:41 +0800

    new scripts for data analysis and visualization

---
+++ functions/LGW_function_plotly_boxplots.R
@@ -0,0 +1,87 @@
+# boxplot function
+
+#log_data = TRUE/FALSE
+
+generic_boxplot <- function(FUNC_boxplot_data, FUNC_boxplot_class, FUNC_metabolite_list, log_data){
+  require(metabom8)
+  require(RColorBrewer)
+  require(tidyverse)
+  require(plotly)
+  
+  bp_plotlist <- list()
+  
+  for(idx_metabolite in FUNC_metabolite_list){
+    
+    #browser()
+    # prepare plot_ly data
+    temp_plot_data <- FUNC_boxplot_data %>%
+      select(all_of(FUNC_boxplot_class), all_of(idx_metabolite)) %>%
+      rename(concentration = all_of(idx_metabolite), 
+             plotClass = all_of(FUNC_boxplot_class))
+    
+    if(log_data == TRUE){
+      temp_plot_data$concentration <- log(temp_plot_data$concentration+1)
+    }
+    
+    
+    # repare plot_ly attributes
+    plot_colors <- RColorBrewer::brewer.pal(name = "Set3",
+                                            n = length(unique(temp_plot_data$plotClass)))
+    
+    
+    title_text <- paste0(idx_metabolite)
+    
+    x_axis_settings_scores <- list(
+      zeroline = TRUE,
+      showline = TRUE,
+      linecolor = toRGB("black"),
+      linewidth = 2,
+      showgrid = TRUE,
+      title = paste("cohort")
+    )
+    
+    y_axis_settings_scores <- list(
+      zeroline = TRUE,
+      showline = TRUE,
+      linecolor = toRGB("black"),
+      linewidth = 2,
+      showgrid = TRUE,
+      title = paste("concentration")
+    )
+    
+    if(log_data == TRUE){
+    y_axis_settings_scores <- list(
+      zeroline = TRUE,
+      showline = TRUE,
+      linecolor = toRGB("black"),
+      linewidth = 2,
+      showgrid = TRUE,
+      title = paste("concentration (LOG)")
+    )
+    }
+    
+    # create plot_ly
+    bp <- plot_ly(type = "box",
+                  #mode = "markers",
+                  temp_plot_data, 
+                  x = ~plotClass, 
+                  y = ~concentration, 
+                  color = ~plotClass,
+                  colors = c(plot_colors[1:length(unique(temp_plot_data$plotClass))]), 
+                  marker = list(size = 7, 
+                                opacity = 0.5,
+                                line = list(color = '#000000',
+                                            width = 1)
+                  )
+    ) %>% 
+      layout(title = paste0(title_text),
+             xaxis = x_axis_settings_scores,
+             yaxis = y_axis_settings_scores)
+    
+   
+    bp_plotlist[idx_metabolite] <- list(bp)
+  }
+  
+  bp_plotlist
+}
+
