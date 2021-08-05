45e3a0df195df341c499e5bd08bbf7bf94ab8547
Luke Whiley <luke.whiley@murdoch.edu.au>
2021-08-05 16:11:48 +0800

    Create 2021-LGW-universal_OPLS_function.R

---
+++ functions/2021-LGW-universal_OPLS_function.R
@@ -0,0 +1,141 @@
+#ANPC Lipidomics opls quality control visualisation
+
+# FUNC_individual_multivariate_data = data containing individual lipid data - MUST CONTAIN sampleID column 
+# FUNC_colour_by = how to colour the plot (e.g. sample class, is_ltr or cohort)
+# FUNC_plot label = what to label the scores plot with (e.g. sampleID)
+# FUNC_scaling = UV or Pareto
+
+lgw_opls <- function(FUNC_individual_multivariate_data, FUNC_opls_y, FUNC_metabolite_list, FUNC_colour_by, FUNC_plot_label, FUNC_scaling){
+  require(metabom8)
+  require(RColorBrewer)
+  require(tidyverse)
+  require(plotly)
+  
+  #browser()
+  
+  #create data matrix for opls
+  opls_x <- FUNC_individual_multivariate_data %>%  select(all_of(FUNC_metabolite_list)) %>% as.matrix()+1 
+  opls_x <- log(opls_x)
+  title_text <- "opls"
+  opls_x[opls_x == 0] <- NA #remove all 0 values
+  opls_x[is.infinite(opls_x)] <- NA #remove all infinite values
+  min_value <- min(opls_x, na.rm = TRUE) # find the lowest value in the matrix
+  opls_x[is.na(opls_x)] <- min_value # replace all NA, Inf, and 0 values with the lowest value in the matrix
+  
+  #create opls model
+  opls_model <- opls(X = opls_x, 
+                     Y = FUNC_opls_y,
+                     scale = paste(FUNC_scaling), 
+                     center = TRUE)
+  
+  # extract score values for plotting in plot_ly
+  PC1 <- as.numeric(as.matrix(opls_model@t_pred))
+  PC2 <- as.numeric(as.matrix(opls_model@t_orth))
+  
+  # extract loadings values for plotting in plot_ly
+ 
+    eruption_model <- eruption(mod = opls_model, 
+                             pc = 1,
+                             p_adj = "BH")
+    
+    plotly_loadings_data <- eruption_model$data %>% as_tibble() 
+  
+  #produce plot_ly opls scores plot
+  
+  # set plot attributes (controlled by FUNC_colour_by and FUNC_plot_label)
+  opls_colour <- FUNC_individual_multivariate_data %>% select(all_of(FUNC_colour_by)) %>% as.matrix()
+  opls_colour[is.na(opls_colour)] <- "none"
+  opls_plot_label <- FUNC_individual_multivariate_data %>% 
+    select(all_of(FUNC_plot_label)) %>% 
+    as.matrix()
+  
+  # create plot values
+  plot_Val <- as_tibble(cbind(PC1, PC2))
+  plot_Val$opls_colour <- c(opls_colour)
+  plot_Val$opls_plot_label <- c(opls_plot_label)
+  
+  plot_colors <- RColorBrewer::brewer.pal(name = "Set3",
+                                          n = length(unique(opls_colour)))
+  
+  x_axis_settings_scores <- list(
+    zeroline = TRUE,
+    showline = TRUE,
+    linecolor = toRGB("black"),
+    linewidth = 2,
+    showgrid = TRUE,
+    title = paste("PC1 (", round(opls_model@Parameters$R2[1]*100,1), " %)", sep = "")
+  )
+  
+  y_axis_settings_scores <- list(
+    zeroline = TRUE,
+    showline = TRUE,
+    linecolor = toRGB("black"),
+    linewidth = 2,
+    showgrid = TRUE,
+    title = paste("PC2 (", round(opls_model@Parameters$R2[2]*100,1), " %)", sep = "")
+  )
+  
+ plotly_opls <- plot_ly(type = "scatter", 
+                       mode = "markers", 
+                       data = plot_Val, 
+                       x = ~PC1, 
+                       y = ~PC2, 
+                       text = ~opls_plot_label, 
+                       color = ~opls_colour, 
+                       colors = c(plot_colors[1:length(unique(opls_colour))]), 
+                        marker = list(size = 7, 
+                                      #color = '#1E90FF', 
+                                      opacity = 0.5,
+                                      line = list(
+                                        color = '#000000',
+                                        width = 1)
+                        )) %>% 
+    layout(title = paste(" Plotly opls - ", title_text, sep = ""),
+           xaxis = x_axis_settings_scores,
+           yaxis = y_axis_settings_scores)
+  
+ 
+ 
+ # create loadings plot
+  x_axis_settings_loading <- list(
+    zeroline = TRUE,
+    showline = TRUE,
+    linecolor = toRGB("black"),
+    linewidth = 2,
+    showgrid = TRUE,
+    title = paste("")
+  )
+  
+  y_axis_settings_loading <- list(
+    zeroline = TRUE,
+    showline = TRUE,
+    linecolor = toRGB("black"),
+    linewidth = 2,
+    showgrid = TRUE,
+    title = paste("")
+  )
+  
+  plotly_loadings <- plot_ly(type = "scatter", 
+                             mode = "markers", 
+                             data = plotly_loadings_data, 
+                             x = ~Cd, 
+                             y = ~p1, 
+                             text = ~id, 
+                             marker = list(size = 7, color = '#808080', opacity = 0.5,
+                                           line = list(color = '#000000', width = 1)
+                             )) %>% 
+    layout(title = paste(" Plotly opls - ", title_text, sep = ""),
+           xaxis = x_axis_settings_loading,
+           yaxis = y_axis_settings_loading
+    )
+  
+  combined_plotly <- subplot(plotly_opls, plotly_loadings, 
+                             margin = c(0.05, 0.05, 0.01, 0.01),
+                             titleX = TRUE,
+                             titleY = TRUE
+  ) %>% layout(showlegend = TRUE, title =  "")
+  
+
+  combined_plotly
+
+}
