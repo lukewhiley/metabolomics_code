3ca6314de3850419139f9ed4dfc33427c830c130
Luke Whiley <luke.whiley@murdoch.edu.au>
2022-06-27 20:05:24 +0800

    Update 2022_LGW_FUNC_MRM_findeR.R

--- functions/2022_LGW_FUNC_MRM_findeR.R
+++ functions/2022_LGW_FUNC_MRM_findeR.R
@@ -1,8 +1,9 @@
 # RT findeR
-function(FUNC_data_path,
+test_func <- function(FUNC_data_path,
          FUNC_mrm_guide,
          FUNC_OPTION_qc_type,
          FUNC_OPTION_max_qc_replicates){
+  #browser()
   require(MSnbase)
   #list mzML files
   mzML_filelist <- list.files(FUNC_data_path, pattern = ".mzML") %>% as_tibble() %>% filter(grepl(paste0(FUNC_qc_type), value)) %>% filter(!grepl("conditioning", value)) %>% filter(!grepl("blank", value))
