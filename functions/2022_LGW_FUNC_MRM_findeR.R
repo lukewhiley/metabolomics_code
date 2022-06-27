0cb097e3704f7cd0e7b1d13c537fe3df1a7d0d4e
Luke Whiley <luke.whiley@murdoch.edu.au>
2022-06-27 19:47:48 +0800

    Update 2022_LGW_FUNC_MRM_findeR.R

--- functions/2022_LGW_FUNC_MRM_findeR.R
+++ functions/2022_LGW_FUNC_MRM_findeR.R
@@ -6,7 +6,7 @@ function(FUNC_data_path,
   require(MSnbase)
   #list mzML files
   mzML_filelist <- list.files(FUNC_data_path, pattern = ".mzML") %>% as_tibble() %>% filter(grepl(paste0(FUNC_qc_type), value)) %>% filter(!grepl("conditioning", value)) %>% filter(!grepl("blank", value))
-  
+  FUNC_mrm_guide$explicit_retention_time_2 <- FUNC_mrm_guide$explicit_retention_time
   #if number of mzML qcs > FUNC_OPTION_max_qc_replicates samples in the mzML filelist then take a quartile sample of total LTRs
   if(length(mzML_filelist$value) > FUNC_OPTION_max_qc_replicates) {
     mzML_filelist_idx <- c(seq(1, nrow(mzML_filelist), by = floor(nrow(mzML_filelist)/FUNC_OPTION_max_qc_replicates)), nrow(mzML_filelist))
@@ -18,40 +18,46 @@ function(FUNC_data_path,
     mzML_filelist_crop <- mzML_filelist
   }
   
-  # find RT
-  for(mzML_idx in mzML_filelist_crop$value){
-    test_spectra <- MSnbase::readSRMData(paste0(FUNC_data_path, "/", mzML_idx)) #read in mzML file [mzML_idx]
+  #read in mzml files into a list
+  FUNC_spectra <- list()
+  for(idx_mzML in mzML_filelist_crop$value){
+    FUNC_spectra[[idx_mzML]] <- MSnbase::readSRMData(paste0(FUNC_data_path, "/", idx_mzML)) #read in mzML file [mzML_idx]
+  }
     
-    rt_find <- NULL
+rt_find <- NULL
     #for each mrm transtion in the transition data
-    for (transition_idx in 1:nrow(test_spectra)){
+for (idx_mrm in 1:nrow(FUNC_mrm_guide)){
       # find precursor reference
-      precursor_mz <- test_spectra[transition_idx,]@precursorMz[1] %>% 
+      precursor_mz <- FUNC_mrm_guide$precursor_mz[idx_mrm] %>% 
         round(2) 
       #find product ion reference
-      product_mz <- test_spectra[transition_idx,]@productMz[1] %>% 
+      product_mz <- FUNC_mrm_guide$product_mz[idx_mrm] %>% 
         round(2) 
-      # find median max intensity scan from all of the QCs
-      max_intensity_idx <- which(test_spectra[transition_idx,]@intensity == max(test_spectra[transition_idx,]@intensity)) %>% 
-        median()
-      #find # of scans in window
-      scans_in_window <- length(test_spectra[transition_idx,]@rtime) 
-      #find apex scan of RT
-      apex_rt <- test_spectra[transition_idx,]@rtime[max_intensity_idx] 
-      # find lipid name from tempalte
-      precursor_name <- FUNC_mrm_guide$precursor_name[which(round(FUNC_mrm_guide$precursor_mz,2) == precursor_mz & round(FUNC_mrm_guide$product_mz,2) == product_mz)] %>% 
-        unique() 
       
-      if(length(precursor_name) == 1){
-        FUNC_mrm_guide$explicit_retention_time_2 <- FUNC_mrm_guide$explicit_retention_time
-        FUNC_mrm_guide$explicit_retention_time_2[which(FUNC_mrm_guide$precursor_name == precursor_name)] <- round(apex_rt, 2)
+      #find transition in each mzML file and find median peak apex
+      mzml_rt_apex_out <- NULL
+      for(idx_mzml in names(FUNC_spectra)){
+        #find the data channel in the mzml file containing the data
+      idx_mrm_channel <- which(
+        FUNC_spectra[[idx_mzml]]@featureData@data$precursorIsolationWindowTargetMZ == precursor_mz &
+          FUNC_spectra[[idx_mzml]]@featureData@data$productIsolationWindowTargetMZ == product_mz)
+      #only complete the below if idx_mrm_channel finds a single unique match
+      if(length(idx_mrm_channel) ==1){
+      #find scan index of max intensity within mrm channel
+      mzml_max_intensity <- which.max(FUNC_spectra[[idx_mzml]][idx_mrm_channel]@intensity)
+      #find rt_peak_apex
+      mzml_rt_apex <- FUNC_spectra[[idx_mzml]][idx_mrm_channel]@rtime[mzml_max_intensity] %>% round(2)
+      #c() mzml rt apex from all mzml
+      mzml_rt_apex_out <- c(mzml_rt_apex_out, mzml_rt_apex)
+      mzml_median_rt <- median(mzml_rt_apex_out)
+      FUNC_mrm_guide$explicit_retention_time_2[idx_mrm] <- round(mzml_median_rt, 2)
       }
-    
-      detach("package:MSnbase", unload = TRUE)
-      
-    }
-  }
-  FUNC_mrm_guide 
+      }
+}
+  #detach MSnbase package
+  detach("package:MSnbase", unload = TRUE)
+  #output finalk table
+  FUNC_mrm_guide
 }
     
     
