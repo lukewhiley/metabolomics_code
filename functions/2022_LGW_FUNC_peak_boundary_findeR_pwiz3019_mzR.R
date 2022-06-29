db98e302ff1262df8122d9415e09c9bf7946ecae
Luke Whiley <luke.whiley@murdoch.edu.au>
2022-06-29 11:35:32 +0800

    Create 2022_LGW_FUNC_peak_boundary_findeR_pwiz3019_mzR.R

---
+++ functions/2022_LGW_FUNC_peak_boundary_findeR_pwiz3019_mzR.R
@@ -0,0 +1,42 @@
+# RT findeR
+peak_boundary_findR <- function(FUNC_data_path,
+                          FUNC_mrm_guide,
+                          FUNC_OPTION_qc_type,
+                          FUNC_OPTION_max_qc_replicates){
+
+
+FUNC_data <- read_csv(FUNC_data_path, show_col_types = FALSE) %>% clean_names()
+FUNC_filenames <- FUNC_data$replicate %>% unique()
+FUNC_metabolite <- unique(FUNC_data$peptide)
+
+#filter only LTRs or PQC
+if(FUNC_OPTION_qc_type == "LTR"| FUNC_OPTION_qc_type == "PQC"){
+  FUNC_data_qc <- FUNC_data %>% filter(grepl(paste0(FUNC_OPTION_qc_type), replicate))
+}
+
+#if no LTR or PQC is present use all samples
+if(FUNC_OPTION_qc_type == "none"){
+  FUNC_data_qc <- FUNC_data
+}
+
+FUNC_data_qc$area <- sapply(FUNC_data_qc$area, as.numeric) #ensure area column is numeric
+rt_boundary_output <- lapply(FUNC_metabolite, function(FUNC_LIPID){
+  #browser()
+  #print(FUNC_LIPID)
+  rt_boundary <-  filter(FUNC_data_qc, peptide == FUNC_LIPID) %>% filter(!is.na(area)) %>% filter(!grepl("conditioning", replicate)) %>% arrange(retention_time)
+  if(nrow(rt_boundary) > 0) {
+    start_time <- rt_boundary %>% select(start_time) %>% sapply(as.numeric) %>% min()
+    end_time <- rt_boundary %>% select(end_time) %>% sapply(as.numeric) %>% max()
+   
+    rt_boundary_filelist <- FUNC_filenames %>% as_tibble() %>% rename(FileName = value)
+    rt_boundary_filelist$FullPeptideName <- FUNC_LIPID
+    rt_boundary_filelist$MinStartTime <- round(start_time,2)
+    rt_boundary_filelist$MaxEndTime <- round(end_time, 2)
+    rt_boundary_filelist
+  }
+}) %>% bind_rows
+
+rt_boundary_output <- rt_boundary_output %>% filter(!is.na(MinStartTime)) %>% filter(!is.na(MaxEndTime)) #remove any NAs that are in the peak boundary guide
+
+rt_boundary_output
+}
