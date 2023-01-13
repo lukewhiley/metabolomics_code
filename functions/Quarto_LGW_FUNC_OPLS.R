2664492f71965aa2ed641a723e3a2a4ca64b8389
Luke Whiley <luke.whiley@murdoch.edu.au>
2023-01-13 18:11:55 +0800

    updates

--- functions/Quarto_LGW_FUNC_OPLS.R
+++ functions/Quarto_LGW_FUNC_OPLS.R
@@ -70,19 +70,21 @@ for(FUNC_idx_str_opls in FUNC_OPLS_comparison_test){
   #set random seed for reproducibility
   set.seed(123)
   #create opls model
-  capture.output(
-    suppressMessages(
+  #capture.output(
+   # suppressMessages(
+  #sink("/Users/lukegraywhiley/Documents/sink_file/file.txt")
       opls_output[[FUNC_idx_str_opls]]$opls_model <- 
     metabom8:: opls(X = opls_x, 
          Y = opls_y,
          scale = paste(FUNC_OPTION_scaling),
          center = TRUE,
          maxPCo = (FUNC_OPTION_max_orth +1))
-  ))
+      #sink()
+  #))
   
   # extract score values for plotting in plot_ly
-  PC1 <- as.numeric(as.matrix(opls_output[[FUNC_idx_str_opls]]$opls_model@t_pred))
-  PC2 <- as.numeric(as.matrix(opls_output[[FUNC_idx_str_opls]]$opls_model@t_orth))
+  tPred <- as.numeric(as.matrix(opls_output[[FUNC_idx_str_opls]]$opls_model@t_pred))
+  tOrth <- as.numeric(as.matrix(opls_output[[FUNC_idx_str_opls]]$opls_model@t_orth))
   
   # extract loadings values for plotting in plot_ly
  
@@ -121,9 +123,12 @@ for(FUNC_idx_str_opls in FUNC_OPLS_comparison_test){
     as.matrix() %>% c()
   
   # create plot values
-  plot_Val <- as_tibble(cbind(PC1, PC2))
+  plot_Val <- as_tibble(cbind(tPred, tOrth))
   plot_Val$opls_colour <- opls_colour
   plot_Val$opls_plot_label <- opls_plot_label
+  
+  #report if control is left (neg) or right (pos) of scores plot - helps contorl plotting later in function
+  control_direction <- (plot_Val %>% filter(opls_colour == FUNC_OPLS_comparison_control))[["tPred"]] %>% median()
   
   #scores axis settings
   x_axis_settings_scores <- list(
@@ -152,8 +157,8 @@ for(FUNC_idx_str_opls in FUNC_OPLS_comparison_test){
   opls_output[[FUNC_idx_str_opls]]$plot_scores <- plot_ly(type = "scatter", 
                        mode = "markers", 
                        data = plot_Val, 
-                       x = ~PC1, 
-                       y = ~PC2, 
+                       x = ~tPred, 
+                       y = ~tOrth, 
                        text = ~opls_plot_label, 
                        color = ~opls_colour, 
                        colors = FUNC_OPTION_project_colours, 
@@ -169,23 +174,28 @@ for(FUNC_idx_str_opls in FUNC_OPLS_comparison_test){
       xaxis = x_axis_settings_scores,
       yaxis = y_axis_settings_scores,
       margin = list(l = 65, r = 50, b=65, t=85),
-      title = paste0(FUNC_OPTION_title, ": OPLS-DA Scores: ", FUNC_OPLS_comparison_control, " vs ", FUNC_idx_str_opls, "\n", nrow(plot_Val), " samples; ", nrow(plotly_eruption_data), " features; R2X = ", 
+      title = paste0(FUNC_OPTION_title, "OPLS-DA Scores: ", FUNC_OPLS_comparison_control, " vs ", FUNC_idx_str_opls, "\n", nrow(plot_Val), " samples; ", nrow(plotly_eruption_data), " features; R2X = ", 
                      signif(opls_output[[FUNC_idx_str_opls]]$opls_model@summary$R2X[1],2)
       )
     )
   
+  #invert axis to keep control on lefthand side of the plot
+  if(control_direction > 0){
+    opls_output[[FUNC_idx_str_opls]]$plot_scores <- opls_output[[FUNC_idx_str_opls]]$plot_scores %>%
+      layout(xaxis = list(autorange = "reversed"))
+  }
+  
+  #options to invert axis
   if(FUNC_OPTION_invert_x == TRUE){
-    pca_output$plot_scores <- pca_output$plot_scores %>%
+    opls_output[[FUNC_idx_str_opls]]$plot_scores <- opls_output[[FUNC_idx_str_opls]]$plot_scores %>%
       layout(xaxis = list(autorange = "reversed"))
   }
   
   if(FUNC_OPTION_invert_y == TRUE){
-    pca_output$plot_scores <- pca_output$plot_scores %>%
+    opls_output[[FUNC_idx_str_opls]]$plot_scores <- opls_output[[FUNC_idx_str_opls]]$plot_scores %>%
       layout(yaxis = list(autorange = "reversed"))
   }
   
-  # 
-
  # create loadings plot
   x_axis_settings_loading <- list(
     zeroline = TRUE,
@@ -226,11 +236,19 @@ for(FUNC_idx_str_opls in FUNC_OPLS_comparison_test){
       yaxis = y_axis_settings_loading,
       showlegend = TRUE, 
       margin = list(l = 65, r = 50, b=65, t=85),
-      title = paste0(FUNC_OPTION_title, ": OPLS-DA Loadings: ", FUNC_OPLS_comparison_control, " vs ", FUNC_idx_str_opls, "\n", nrow(plot_Val), " samples; ", nrow(plotly_eruption_data), " features; R2X = ", 
+      title = paste0(FUNC_OPTION_title, "OPLS-DA Loadings: ", FUNC_OPLS_comparison_control, " vs ", FUNC_idx_str_opls, "\n", nrow(plot_Val), " samples; ", nrow(plotly_eruption_data), " features; R2X = ", 
                      signif(opls_output[[FUNC_idx_str_opls]]$opls_model@summary$R2X[1],2)
       )
     )
 
+  
+  #invert axis to keep control on lefthand side of the plot
+  if(control_direction > 0){
+    opls_output[[FUNC_idx_str_opls]]$plot_loadings <- opls_output[[FUNC_idx_str_opls]]$plot_loadings %>%
+      layout(xaxis = list(autorange = "reversed"))
+  }
+  
+  
   
   opls_output[[FUNC_idx_str_opls]]$plot_combined <- subplot(
     opls_output[[FUNC_idx_str_opls]]$plot_scores, 
@@ -243,16 +261,55 @@ for(FUNC_idx_str_opls in FUNC_OPLS_comparison_test){
     layout(
       showlegend = TRUE, 
       margin = list(l = 65, r = 50, b=65, t=85),
-      title = paste0(FUNC_OPTION_title,": OPLS-DA: ", FUNC_OPLS_comparison_control, " vs ", FUNC_idx_str_opls,  "\n", nrow(plot_Val), " samples; ", nrow(plotly_eruption_data), " features; R2X = ", 
+      title = paste0(FUNC_OPTION_title,"OPLS-DA: ", FUNC_OPLS_comparison_control, " vs ", FUNC_idx_str_opls,  "\n", nrow(plot_Val), " samples; ", nrow(plotly_eruption_data), " features; R2X = ", 
                      signif(opls_output[[FUNC_idx_str_opls]]$opls_model@summary$R2X[1],2)
                      )
     )
 
   opls_output[[FUNC_idx_str_opls]]$data_eruption <- plotly_eruption_data %>% arrange(desc(p1))
 
+  # create loadings barchart for visualisation
+  #extract loadings data from model
+  plotly_loadings_data <- tibble(metabolite = colnames(opls_output[[FUNC_idx_str_opls]]$opls_model@X),
+                                 loading = opls_output[[FUNC_idx_str_opls]]$opls_model@w_pred)
+  #create list of top 20 important features
+  plotly_loadings_data_crop <- plotly_loadings_data %>%
+    arrange(loading) %>%
+    {bind_rows(slice_head(., n=10), slice_tail(., n=10))} 
   
+  #reverse order of barchart if control plots on OPLS-DA right (pos of plot)
+  if(control_direction > 0){plotly_loadings_data_crop <- plotly_loadings_data_crop %>%
+    arrange(desc(loading))}
   
+  #add column for plot colour
+  plotly_loadings_data_crop[["colour"]] <- "pos"
+  plotly_loadings_data_crop[["colour"]][1:10] <- "neg"
   
+  #set metabolite names as factor for control of plotting
+  plotly_loadings_data_crop$metabolite <- plotly_loadings_data_crop$metabolite %>%
+    factor(levels = plotly_loadings_data_crop$metabolite, ordered = TRUE)
+  
+  #create bar plot
+  bp <- ggplot(data=plotly_loadings_data_crop, 
+               aes(x=metabolite, y=loading, fill = colour)) +
+    geom_bar(stat="identity", colour = "black")
+  bp <- bp + labs(x = paste(""), y = paste("OPLS-DA\nPredictive loading"))
+  bp <- bp +  ggtitle("")
+  bp <- bp +  theme_cowplot() 
+  bp <- bp +  theme(plot.title = element_text(hjust = 0.5)) 
+  bp <- bp +  theme(plot.title = element_text(size=12)) 
+  bp <- bp +  theme(axis.text.y = element_text(size = 10, margin = margin(t = 0, r = 0, b = 0, l = 2)))
+  bp <- bp +  theme(axis.text.x = element_text(size = 10, angle = 45, vjust = 1, hjust = 1))
+  bp <- bp + theme(axis.title = element_text(size = 12)) 
+  bp <- bp + scale_fill_manual(values = FUNC_OPTION_project_colours[
+    c(which(levels(FUNC_data[[FUNC_OPTION_colour_by]]) == FUNC_OPLS_comparison_control),
+      which(levels(FUNC_data[[FUNC_OPTION_colour_by]]) == FUNC_idx_str_opls))])
+  bp <- bp + scale_color_manual(values = c("black"))
+  bp <- bp + theme(legend.position = "none") 
+  
+  if(control_direction > 0){bp <- bp + scale_y_reverse()}
+  
+  opls_output[[FUNC_idx_str_opls]]$loading_bar <- bp %>% plotly::ggplotly()
 
   }
   
